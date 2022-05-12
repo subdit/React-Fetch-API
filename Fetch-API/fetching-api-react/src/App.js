f4da976b0069b0e72c3458c88aa5a668623cab8d
Kasidit Bualoeng <subdit@gmail.com>
2022-05-12 11:33:35 -0400

    freshly start a fetch-api app

---
+++ Fetch-API/fetching-api-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header>
+      <h2>Hello..Fetch Api using React</h2>
+    </header>
+  );
+}
+
+export default App;
